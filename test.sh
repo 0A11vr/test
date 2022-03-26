f13673da84c611a3b698ad5a1e325e8d4f67924c
0A11vr <40143063+0A11vr@users.noreply.github.com>
2022-03-26 03:48:58 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-echo "Hello, world!"
-read -p "What is your name? " name
-echo "Hello, ${name}!"
+staff=("doe_j" "hitchcock_a" "joplin_j")
+mkdir -p /data/staff && for i in ${staff[@]}; do mkdir -p /data/home/$i;done
+for i in ${staff[@]}; do adduser $i; done
+for i in ${staff[@]}; do usermod -a -G staff $i; done
+adduser connor_s
+usermod -a -G sudo connor_s
+chgrp staff /data/staff/
+chmod -R 070 /data/staff/
+for i in ${staff[@]}; do echo '"Hello World!"' > /data/home/$i/mysecretfile; done
+for i in ${staff[@]}; do chmod -R 700 /data/home/$i; done
+cat /etc/group | grep staff > /home/connor_s/staff
+chown connor_s /home/connor_s/staff
