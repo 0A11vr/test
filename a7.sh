572400ca777afee4a2944346b7d1e6742e446382
0A11vr <40143063+0A11vr@users.noreply.github.com>
2022-03-26 05:53:46 +0000

    Update a7.sh

--- a7.sh
+++ a7.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 staff=("doe_j" "hitchcock_a" "joplin_j")
+badpass='asdfASDF1234!@#$'
 mkdir -p /data/staff
 for i in "${staff[@]}"; do 
         mkdir -p /data/home/"$i"
         useradd -m -G staff "$i"
+        echo -e "$badpass\n$badpass" | passwd "$i"
         echo '"Hello World!"' > /data/home/"$i"/mysecretfile
         chown -R "$i" /data/home/"$i"
         chmod -R 700 /data/home/"$i"
 done
 chgrp staff /data/staff/
 chmod -R 070 /data/staff/
+useradd -m -G sudo connor_s
+echo -e "$badpass\n$badpass" | passwd "$i"
 cat /etc/group | grep staff > /home/connor_s/staff
 chown connor_s /home/connor_s/staff
