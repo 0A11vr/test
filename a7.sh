363cb765f07ed66a57ad52e99268ef5881048745
0A11vr <40143063+0A11vr@users.noreply.github.com>
2022-03-26 05:36:36 +0000

    Update a7.sh

--- a7.sh
+++ a7.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-staff=("doe_j" "hitchcock_a" "joplin_j" "connor_s")
+staff=("doe_j" "hitchcock_a" "joplin_j")
 mkdir -p /data/staff
 for i in "${staff[@]}"; do 
-        if [ "$i" = "connor_s" ]; then
-                continue
-        else
-                mkdir -p /data/home/"$i"
-        fi
-done
-for i in "${staff[@]}"; do 
-        if [ "$i" = "connor_s" ]; then
-                useradd -m -G sudo "$i"; else
+        mkdir -p /data/home/"$i"
         useradd -m -G staff "$i"
-        fi
-done
-chgrp staff /data/staff/
-chmod -R 070 /data/staff/
-for i in "${staff[@]}"; do
         echo '"Hello World!"' > /data/home/"$i"/mysecretfile
         chown -R "$i" /data/home/"$i"
         chmod -R 700 /data/home/"$i"
 done
+chgrp staff /data/staff/
+chmod -R 070 /data/staff/
 cat /etc/group | grep staff > /home/connor_s/staff
 chown connor_s /home/connor_s/staff
