443b0edead4553e61ee2f680f36e89e237374c7c
0A11vr <40143063+0A11vr@users.noreply.github.com>
2022-03-26 04:37:11 +0000

    Create a7.sh

---
+++ a7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+staff=("doe_j" "hitchcock_a" "joplin_j" "connor_s")
+mkdir -p /data/staff && for i in "${staff[@]}"; do mkdir -p /data/home/"$i";done
+for i in "${staff[@]}"; do 
+        if "${staff[@]}" -eq "connor_s"; then
+                useradd -G sudo "$i"; else
+        useradd -G staff "$i"
+        fi
+done
+chgrp staff /data/staff/
+chmod -R 070 /data/staff/
+for i in "${staff[@]}"; do echo '"Hello World!"' > /data/home/"$i"/mysecretfile; done
+for i in "${staff[@]}"; do chmod -R 700 /data/home/"$i"; done
+cat /etc/group | grep staff > /home/connor_s/staff
+chown connor_s /home/connor_s/staff
